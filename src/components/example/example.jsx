30e20680c3ffad02f6691a66da8687f8c8d84344
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-08-27 02:34:21 +0500

    Add img sizes

--- src/components/example/example.jsx
+++ src/components/example/example.jsx
@@ -5,7 +5,7 @@ const Example = () => {
     <section className="container">
       <div className="wrapper">
         <h1>Webpack</h1>
-        <img src="assets/img/batman.jpg" alt=""/>
+        <img src="assets/img/batman.jpg" alt="I'm Batman!" width="479" height="628"/>
       </div>
     </section>
   );
